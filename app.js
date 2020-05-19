83448ef4b6a10e6e2b496c9f7a0e21433eb95a17
Chris Song <chris.bk.song@gmail.com>
2020-05-19 10:36:43 -0400

    adds express practice 3

---
+++ app.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false}));
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.render('index', {
+    title: 'My Website',
+  });
+});
+
+app.get('/contact', (req, res) => {
+  res.render('contact', {
+    title: "Contact me. I won't bite.",
+    submitted: false,
+  });
+});
+
+app.post('/contact', (req, res) => {
+  // what happens here is up to you
+  // send back confirmation email
+  // print out thank you message
+  console.log(req.body);
+  res.render('contact', {
+    title: 'Thank You!!!',
+    submitted: true,
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening. Open http://localhost:${PORT} to view.`);
+});
